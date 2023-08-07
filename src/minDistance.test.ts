2d9c3487c90afe526415441f4dcf7e374d15f2ad
TomOwen93 <mr.tsowen@gmail.com>
2023-08-07 10:20:18 +0100

    feat tests for numbers less than 1

--- src/minDistance.test.ts
+++ src/minDistance.test.ts
@@ -6,3 +6,9 @@ test("minDistance finds the smallest difference between two factors of a number"
   expect(minDistance(25)).toBe(4);
   expect(minDistance(218683)).toBe(198);
 });
+
+test("minDistance returns undefined if given a number less than or equal to 1", () => {
+  expect(minDistance(1)).toBe(undefined);
+  expect(minDistance(0)).toBe(undefined);
+  expect(minDistance(-1)).toBe(undefined);
+});
--- src/minDistance.ts
+++ src/minDistance.ts
@@ -1,15 +1,19 @@
-export function minDistance(n: number): number {
-  let result = 0;
+export function minDistance(n: number): number | undefined {
+  if (n > 1) {
+    let result = 0;
 
-  const factorArray = [];
-  for (let i = 1; i <= n; i++) {
-    n % i === 0 ? factorArray.push(i) : "";
+    const factorArray = [];
+    for (let i = 1; i <= n; i++) {
+      n % i === 0 ? factorArray.push(i) : "";
+    }
+
+    let resArray = [];
+    let min = n;
+    factorArray.map((num, index, array) =>
+      array[index + 1] - num < min ? (min = array[index + 1] - num) : ""
+    );
+    return min;
+  } else {
+    return undefined;
   }
-
-  let resArray = [];
-  let min = n;
-  factorArray.map((num, index, array) =>
-    array[index + 1] - num < min ? (min = array[index + 1] - num) : ""
-  );
-  return min;
 }
